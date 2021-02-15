84b9ae2cdbcdb93da976fe4911ba7c4b9bcc7db8
Yu leo <yuleo@netscape.net>
2021-02-15 15:49:56 -0800

    added 0.4.0 to podspec, pod, and tag 0.4.0

--- RWPickFlavor/Controllers/PickFlavorViewController.swift
+++ RWPickFlavor/Controllers/PickFlavorViewController.swift
@@ -55,7 +55,7 @@ public class PickFlavorViewController: UIViewController {
   
   public override func viewDidLoad() {
     super.viewDidLoad()
-    print("RWPickFlavor 0.3.0")
+    print("RWPickFlavor 0.4.0")
     collectionView.delegate = self
     loadFlavors()
   }
